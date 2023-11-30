5bae775d618294d226c7b01d67de18924af9d17c
BrunoSantosCode <brunomsantos203@gmail.com>
2023-11-30 11:15:08 +0000

    update

--- bin/run-ansible.sh
+++ bin/run-ansible.sh
@@ -235,7 +235,7 @@ pip3 install --user -U pip
 sudo chown $USER:$USER $aurora_home || true
 sudo rm -rf ${aurora_home}
 
-git clone --depth 1 -b main ${aurora_tools_branch} https://github.com/BrunoSantosCode/aurora.git $aurora_home
+git clone --depth 1 -b ${aurora_tools_branch} https://github.com/BrunoSantosCode/aurora.git $aurora_home
 
 echo ""
 echo " -------------------"
